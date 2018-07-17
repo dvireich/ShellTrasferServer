b6c02185d209a025f9f0a7c2d886a37cac46765d
dvireich <38475641+dvireich@users.noreply.github.com>
2018-07-17 22:21:45 +0300

    simplify

--- ShellTrasferServer/Interfaces/IActiveShell.cs
+++ ShellTrasferServer/Interfaces/IActiveShell.cs
@@ -12,24 +12,34 @@ namespace ShellTrasferServer
     {
         [OperationContract]
         string ActiveNextCommand(string command);
+
         [OperationContract]
         string ActiveClientRun();
+
         [OperationContract]
         RemoteFileInfo ActiveDownloadFile(DownloadRequest request);
+
         [OperationContract]
         RemoteFileInfo ActiveUploadFile(RemoteFileInfo request);
+
         [OperationContract]
         void ClearQueue();
+
         [OperationContract]
         string GetStatus();
+
         [OperationContract]
         bool SelectClient(string id);
+
         [OperationContract]
         bool ActiveCloseClient(string id);
+
         [OperationContract]
         bool ClearAllData(string id);
+
         [OperationContract]
         bool DeleteClientTask(string id, bool shellTask, int taksNumber);
+
         [OperationContract]
         bool ActiveSetNickName(string id, string nickName);
     }
--- ShellTrasferServer/Interfaces/IPassiveShell.cs
+++ ShellTrasferServer/Interfaces/IPassiveShell.cs
@@ -12,29 +12,42 @@ namespace ShellTrasferServer
     {
         [OperationContract]
         bool PassiveClientRun(string id, string taskId, string baseLine);
+
         [OperationContract]
         Tuple<string, string, string> PassiveNextCommand(string id);
+
         [OperationContract]
         void CommandResponse(string id, string taskId, string baseLine);
+
         [OperationContract]
         bool HasCommand(string id);
+
         [OperationContract]
         bool HasUploadCommand(string id);
+
         [OperationContract]
         bool HasDownloadCommand(string id);
+
         [OperationContract]
         void PassiveDownloadedFile(RemoteFileInfo request);
+
         [OperationContract]
         DownloadRequest PassiveGetDownloadFile(DownloadRequest id);
+
         [OperationContract]
         RemoteFileInfo PassiveGetUploadFile(DownloadRequest id);
+
         [OperationContract]
         void PassiveUploadedFile(string id, string taskId);
+
         [OperationContract]
         void ErrorUploadDownload(string id, string taskId, string response);
+
         [OperationContract]
         bool Subscribe(string id, string version, string nickName);
+
         [OperationContract]
         void ErrorNextCommand(string id, string taskId, string response);
+
     }
 }
